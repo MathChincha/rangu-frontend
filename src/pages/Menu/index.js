66a7ee5fa31c3d204e3ec6ab7904d6ce779999c5
MathChincha <mathchincha58@gmail.com>
2021-06-05 14:27:14 -0300

    Alterando tela do Menu e inserindo um PopUp

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,39 +1,69 @@
 import React, { useState } from 'react'
 import styles from './menu.module.scss'
 import Header from '../../components/Header'
+import Popup from '../../components/Popup/Popup'
+
 import BigMac from '../../assets/BigMac.jpg'
 import McChicken from '../../assets/McChicken.png'
 import CocaCola from '../../assets/CocaCola.jpg'
 
 export default function Menu({ history }) {
+    const [isOpenType, setIsOpenType] = useState(false);
+
+
+
+    function togglePopupType() {
+        setIsOpenType(!isOpenType);
+    }
+
+
+
+    const types = [
+        {
+            id: 1,
+            type: 'Hamburguers'
+        },
+        {
+            id: 2,
+            type: 'Drinks'
+        },
+        {
+            id: 3,
+            type: 'Dessert'
+        },
+        {
+            id: 4,
+            type: 'Porções'
+        }
+    ]
 
     const foods = [
         {
-            id: 1,
-            type: 'Prato',
+            id: 5,
+            type: 'Hamburguers',
             foodImg: BigMac,
             dishName: 'Big Mac',
             eta: '5 min',
             price: 'R$ 35.99',
-            description: 'Não existe nada igual. Dois hambúrgueres, alface, queijo e molho especial, cebola e picles num pão com gergelim. O sabor de McDonald’s triplamente delicioso. Com três hambúrgueres de carne 100% bovina, queijo derretido, cebola, picles, ketchup e mostarda. Não existe nada igual. Dois hambúrgueres, alface, queijo e molho especial, cebola e picles num pão com gergelim.',
+            description: "There is nothing like it. Two hamburgers, lettuce, cheese and special sauce, onion and pickles on a sesame bun. The flavor of McDonald's is triply delicious. With three 100% beef burgers, melted cheese, onion, pickles, ketchup and mustard. There is nothing like it. Two hamburgers, lettuce, cheese and special sauce, onion and pickles on a sesame bun.",
         },
         {
-            id: 2,
-            type: 'Prato',
+            id: 6,
+            type: 'Hamburguers',
             foodImg: McChicken,
             dishName: 'McChicken',
             eta: '2 min',
             price: 'R$ 10.90',
-            description: 'O sabor que você adora. Frango empanado e dourado com molho suave e cremoso, acompanhado de alface crocante num pão com gergelim.',
+            description: 'The flavor you love. Breaded and browned chicken with a smooth, creamy sauce, accompanied by crispy lettuce on a sesame bun.',
         },
         {
-            id: 3,
-            type: 'Bebida',
+            id: 7,
+            type: 'Drinks',
             foodImg: CocaCola,
             dishName: 'Coca-Cola',
             eta: '2 min',
             price: 'R$ 5.00',
-            description: 'Coca-Cola geladinha',
+            description: 'A cold Brew',
         },
     ]
 
@@ -47,7 +77,7 @@ export default function Menu({ history }) {
         }
     }
 
-    function sair() {
+    function logoff() {
         history.push('/');
     }
     function menu() {
@@ -62,33 +92,45 @@ export default function Menu({ history }) {
 
     return (
         <>
-            <Header menu={() => menu()} sair={() => sair()} orders={() => orders()} profile={() => profile()} />
+            {
+                isOpenType && <Popup
+                    content={<>
+                        <b>Insert the new category</b>
+                        <input></input>
+                        <div>
+                            <button className={styles.insert} onClick={() => { togglePopupType() }}>Insert New Category</button>
+                            <button className={styles.insert} onClick={() => { togglePopupType() }}>Cancel</button>
+                        </div>
+                    </>}
+                    handleClose={togglePopupType}
+                />
+            }
+            <Header menu={() => menu()} logoff={() => logoff()} orders={() => orders()} profile={() => profile()} />
             <div className={styles.menuContainer}>
+                <button className={styles.newCategory} onClick={() => { togglePopupType() }}>Create New Category</button>
                 <div>
-                    <h1>Pratos</h1>
-                    <ul className="food-list">
-                        {foods.filter(food => food.type === "Prato").map((food) => (
-                            <li key={food.id}>
-                                <img src={food.foodImg} />
-                                <strong>{food.dishName}</strong>
-                                <strong>R${food.price}</strong>
-                            </li>
-                        ))}
-                    </ul>
+                    {types.map((type) => (
+                        <>
+                            <h1 className={styles.title} key={type.id}>{type.type}</h1>
+                            <ul className={styles.foodList} key={type.id}>
+                                <button className={styles.ul}>Disable Category</button>
+                                <button className={styles.ul}>Add Item</button>
+                                {foods.filter(food => food.type === type.type).map((food) => (
+                                    <li className={styles.foodList} key={food.id}>
+                                        <img className={styles.dishImg} src={food.foodImg} alt="Food" />
+                                        <strong className={styles.dishName}>{food.dishName}</strong>
+                                        <strong className={styles.description}>Description: {food.description}</strong>
+                                        <strong className={styles.price}>Price: <strong className={styles.color}>{food.price}</strong></strong>
+                                        <strong className={styles.eta}>Time to Prepare: <strong className={styles.color}>{food.eta}</strong></strong>
+                                        <button className={styles.li1}>Disable Item</button>
+                                        <button className={styles.li2}>Edit Item</button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </>
+                    ))}
                 </div>
-                <div>
-                    <h1>Bebidas</h1>
-                    <ul className="food-list">
-                        {foods.filter(food => food.type === "Bebida").map((food) => (
-                            <li key={food.id}>
-                                <img src={food.foodImg} />
-                                <strong>{food.dishName}</strong>
-                                <strong>R${food.price}</strong>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <button>Adicionar Item</button>
+
             </div>
         </>
     );
