44a142021d10fa6448e19038595468b0ad0e5eb5
Gian Costa <rafa_gian2011@hotmail.com>
2021-12-04 17:55:07 -0300

    update design and relatorios

--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -1,24 +1,25 @@
 import { motion } from "framer-motion";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ChartBar from "../../components/Charts/ChartBar";
 import ChartLine from "../../components/Charts/ChartLine";
 import ChartPie from "../../components/Charts/ChartPie";
 import Header from '../../components/Header';
+import Loading from '../../components/Loading/Popup';
 import styles from './reports.module.scss';
 
 
 
 export default function Reports({ history }) {
 
-    async function handleSubmit(event) {
-        event.preventDefault();
+    const [isLoading, setIsLoading] = useState(false);
 
-        try {
-            history.push('/signup')
-        } catch (err) {
-            alert("Alerta");
-        }
-    }
+
+    useEffect(() => {
+        setIsLoading(true);
+        setTimeout(function () {
+            setIsLoading(false);
+        }, 2000);
+    }, []);
 
     function logoff() {
         history.push('/');
@@ -43,9 +44,9 @@ export default function Reports({ history }) {
     }
 
     return (
-        <>
-            <Header menu={() => menu()} logoff={() => logoff()} orders={() => orders()} profile={() => profile()} employess={() => employess()} tables={() => tables()} reports={() => reports()} />
+        isLoading ? <Loading /> :
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                <Header menu={() => menu()} logoff={() => logoff()} orders={() => orders()} profile={() => profile()} employess={() => employess()} tables={() => tables()} reports={() => reports()} />
                 <div className={styles.reportsContainer}>
                     <div className={styles.graphics}>
                         <ChartLine />
@@ -59,6 +60,6 @@ export default function Reports({ history }) {
                     </div>
                 </div>
             </motion.div>
-        </>
+
     );
 }
